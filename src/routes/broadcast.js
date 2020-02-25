ff9a89795a82719350c27b8ebd28a50f37556527
Thomas Monfre <tmonfre1@gmail.com>
2020-02-25 14:03:20 -0500

    add push notification module with FCM

--- src/routes/broadcast.js
+++ src/routes/broadcast.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import * as notifications from '../services/send_push_notifications';
 
 const router = express();
 
 router.route("/")
     .get((req, res) => {
         res.send("hi");
+    });
+
+router.route("/test")
+    .get((req, res) => {
+        res.send(notifications.sendTestMessage());
     });
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -2,39 +2,44 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
-// import * as admin from 'firebase-admin';
-// import * as firebase from 'firebase';
+import * as admin from 'firebase-admin';
+import * as firebase from 'firebase';
 
 import messageRouter from './routes/message';
 import broadcastRouter from './routes/broadcast';
 
 require('dotenv').config(); // load environment variables
 
-// // initialize firebase account
-// firebase.initializeApp({
-// 	apiKey: 'AIzaSyAouxtAu6-lOTYZGY9XZI81mjju5XpRr44',
-// 	authDomain: 'dali-6amhealth.firebaseapp.com',
-// 	projectId: 'dali-6amhealth',
-// });
+// initialize firebase account
+firebase.initializeApp({
+	apiKey: "AIzaSyCK2Vb2o1y6LC8oJrGCNXDkh5Pno1WbKnw",
+	authDomain: "cs65-safetyassistant.firebaseapp.com",
+	databaseURL: "https://cs65-safetyassistant.firebaseio.com",
+	projectId: "cs65-safetyassistant",
+	storageBucket: "cs65-safetyassistant.appspot.com",
+	messagingSenderId: "824909863874",
+	appId: "1:824909863874:web:054dd687869962a35e545b",
+	measurementId: "G-4GL744QSZ2"
+});
 
-// // initialize firebase admin
-// const serviceAccount = {
-// 	type: 'service_account',
-// 	project_id: 'dali-6amhealth',
-// 	private_key_id: '8d94d06383418ae09e4464685dedda2f80a3f7bd',
-// 	private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDV0nOBbAjavrnt\nlmkuz6c7ApqPbHEfj3OwBkGr+N7YEhzL2IChO4M+lz71UU4n7Svhy2l0JySrTAuA\nYYw8Ymt8xtj2/3Roo0RY8b23oNpFtWYp6Zo/njdw0IAwdsuJPZBvByLOHmF7U9hj\n54OtGq2WGVBrh6CRK00XH4i0U03FeYmc8pklejLh7Hb39C6lCA2o3g32nl9SY42X\nXty7VPCUUufRASjHTwjlMS/09mrNFpRZ0NFDvKktqOPnKWUNNqcoq8TtP/14nxOW\n3BjQ7y5KvrTVE+bvAkqvCPugF2+gtyRJJirL2mbHlxQkREn/w96Vxmsp55WHa23o\npeB9/4+JAgMBAAECggEAEoZvN3FSuxD3LtxvNH0NMhGhrndovaFCFqgUHIAtXm8D\nS63wRwfO8SdNbxeiO9dSi/eBpWyWwriN7ouFCN5GDcN0nMG5Ji+vBj27gCNRheSX\ne14f3Q8qIn4N/STtsXBCCTyHCXCcZYD4KH+watj63RxIWWigwrMLtMsonubwwAEF\nHsGc71rNi8zH4yVk3oWOPQKZPRR1Pgaei24W4UYnrJjLh5oh54a9shb2uuUGUYvC\n6RdszwXfxLdprP+zGWH9+6+YDO+OinVlhMXtlV5o+9k91pm8AgImgoNlNCAp2Zsk\nuyTY/yldDpZk6mfOtXHGoOPzRMVAeik4hmHbuwokmQKBgQDqQwff4sXFT7fvO/ot\ndHWPVmuFmPJbd95qA+jLo9E9NUHv9qGA4FnZJTxnLSAJr0uEj5kkfKXD9XEk+6W0\nOPugLbO2UEmhAox8Hir36ucf72Z56A/4ZLvEpH4+j/8Z48TSupCP9dQr69rYOfCC\n7tSWE+whtURY6eN570M1MDMD7wKBgQDpqd3u3qACJfu8TuZvvsFZU2bMjQa20wq/\nwLMCbCPN8DkW2r/kbGJUl8y18OCDoHMfcQWLH4SZicVMkARz6em4XUazEGamT9ee\nrXfDPa31L98NZVMqhDoOcUC4NxQB+mZD7Y4dOaYYmE+ZNTpjjKEwwwV48a7+Pg1J\nkNus+gjMBwKBgATgKEXMnnkg058zDF3t0pSNI+e/6ytkL0jdk/bJDg3MEfdAltPr\n13Lhh4ROOCB7eYLkyjcsMPcHILe4VRaPKQ2IZfkhaqbjOrFKn3JRfUX44ZUf81Uk\nI5RQKCB9Nr3/M5V/NIsKfsP7QK7HZe4TsnD9MSjycN244HclN6zft/AZAoGAYaFn\nKKJ+asdiGnlBEVm7wc32OuuF7afaaupiIVX709VWlTULTq72mu/eSfr+ZCELWQzJ\nSyr9JYYKGQZnxeLrk+vtWW415bGgnx1N2BcSc0rwQNGgYThkZZpud0oUIwtz5VgB\ntDZdLxOc1qAF6Ihe5VfnFnStMG/O73kGQI0sVcsCgYEAriOrV/ZS2RLI9KP/+KGc\n+skl0gAsIezhD9VHqIpnHpnQGpyQDEOySYyTT9IbczvIppKu3OsebNqeS4cdkTEq\nySlHy4cn0F82k5YxYOFT0AbQk0AF0BcDrmVIzpLUBsiAwsFt9jj+/yKM3JmlAxtE\n38ILLWwLzaECGxaCtn9PXMk=\n-----END PRIVATE KEY-----\n',
-// 	client_email: 'firebase-adminsdk-1i6wz@dali-6amhealth.iam.gserviceaccount.com',
-// 	client_id: '114453036626647824768',
-// 	auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-// 	token_uri: 'https://oauth2.googleapis.com/token',
-// 	auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-// 	client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-1i6wz%40dali-6amhealth.iam.gserviceaccount.com',
-// };
+// initialize firebase admin
+const serviceAccount = {
+	type: "service_account",
+	project_id: "cs65-safetyassistant",
+	private_key_id: "81ac8ea7b571ccadce8a347371c5c6389f552e03",
+	private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC4Morguw/pxs5V\noZYivAZQmew6t1yWhIZvoLmieLkgYpKQx1wu3nUNnsdSm+nEJq7QyJmFn8Cvaf28\nqTY9U+TNZYOoUX5j9GtOCk/kuy3sKDoXs5ixCTQjp3rZZhOUcJp2reg2rl3XWMvH\nF4OnIreW7Hlukwy3rYm7WyF2Ol3ppfZZntAm+tdDUc4JaBKgn1MOKEJb8SKp+ZVt\nRmCWaJi+MeAMDMizJREzzDSqfD5BnyJDU9TFOZsgdTmqV5UF0lL05CjTt5DEEN+V\n7DvMFMMHn+OUL1bLZFgi0Qa7WvxJ87DlePGuT++L7YLSxmpBesj2klQk4rMGfn4u\ni4r/zC/HAgMBAAECggEAVIV4vZuIt0yvFJMTmfeubCDdSaom9bh1GdbmJ7v+svIY\ncA/S6HzSuH/B1dHOaGAZups5+xttpvBbas04otaSuH9a9QsEFKis7oAD3iDX9zQU\nSBj0uVPQVYX99LEGzggHkOrHEgRb8juAMmjDVBe1p672H7uhS815VHwCNEafHl1s\nYlx7k053tFsnYeZeFta/RLepMirFzQUK0NrH3FqIhzxiplHCSB9Y/4crdjYJQ/Du\nzWRGSsS4kJYZbO/GVuxZg+mv0+RQqF684IyeJE4HwS+c/CgHyF8x6EyOCi0unxJT\nU1X3hpUjnD9Sy/IxEev/Z2jsqJ7PEEw7o/tHMkhITQKBgQD7qPOF/EnVhev6phXv\nNGOJAbqtf2jN26uyB+AMV1lZRb29QvOf732gu4EjdwRHQBir8fLh7x85KamDrh9b\nkPmQ/fsYXfw7JLMZu57nHcPKsB4fhuriKgGz+NHFEildpX6A+4Rre/Gx+cLxXRq+\nJxVZXzUgoh4qoCVx0xGk7I674wKBgQC7X8CPM+omC0PjOKPW9ZwQO3hdF3Ds+1Rf\nRCKiP2Nha99eKzeWVUFWFNZT2TU0KFZlparKfEjpHCpVmWSuE0fE06leqWT+TSJI\nmQjnUXHiWOZdKg/KurPUT/FrnTS6g+n3UdaRqKSdJiGjUBhqBZcpEW4U2POAGmuF\ngCdyZiRJzQKBgAZ/Sio/ZTiaCImkGBTg6WD0JnmsPGx0K26hPWuUJb5kSo3EWL3c\nVQX9UUw1GmssgHaP/JFQtyGmrbW0nSKkD41qwTzT0FtGi0kyytt0rUViXqS5uv6z\nzu9ykRfhbCf6ydVSqQyNWqQZqqrImJQoNijkiF6bUOQQ1mal6GK1qMm5AoGBAIMf\nQLt10veIjptluZHmEq5xT4ulqcrNHoAESFBOF5fHS2q0x5OWWcCJmGrY55h2We+L\n/XbxfnxSz3VTgNcoeuLMVAwAcsJa7BDttwNZRV7mY17y3lY6TTnYL9PlEU8snQAL\n7yHDaF7onkWQ+WTIQL3miqhgIhaVZoTn57Lws5hJAoGBALERTdhZF5SV7CwQZ93K\nxrYeHsI/edrcNFr4QBt1YSVuQ5P3M+36V8BdyH+Eaqvb9vrj6Iah/+GMp8714gdk\nRp1XEuX5S+I2Wpcsdugtz9333qYmPYSAunijqA1ZMpAAWG7qr+28rPRgmWxPqfw+\nsWLwrwPD9zvfd6LsXJDAPBuB\n-----END PRIVATE KEY-----\n",
+	client_email: "firebase-adminsdk-iz2si@cs65-safetyassistant.iam.gserviceaccount.com",
+	client_id: "100318699231087321006",
+	auth_uri: "https://accounts.google.com/o/oauth2/auth",
+	token_uri: "https://oauth2.googleapis.com/token",
+	auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+	client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-iz2si%40cs65-safetyassistant.iam.gserviceaccount.com"
+  };
 
-// admin.initializeApp({
-// 	credential: admin.credential.cert(serviceAccount),
-// 	databaseURL: 'https://dali-6amhealth.firebaseio.com',
-// });
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://cs65-safetyassistant.firebaseio.com',
+});
 
 // initialize server
 const app = express();
